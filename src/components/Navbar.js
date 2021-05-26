6f3e349e5c77d05d7a22643fd64fd55f93885edf
Godswilldev <gimmex1@gmail.com>
2021-05-26 22:07:26 +0100

    fixed errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { NavLink, Link, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Buttons from "./Buttons";
 import logo from "../images/logo.svg";
 class Navbar extends Component {
@@ -15,38 +15,38 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="nav">
-        <NavLink className="nav__logo" exact to="/">
+        <Link className="nav__logo" exact to="/">
           <img src={logo} alt="nav-logo" />
-        </NavLink>
+        </Link>
 
         <div className={this.state.active ? "nav__left activee" : "nav__left"}>
-          <NavLink className="nav__link" exact to="/">
+          <Link className="nav__link" exact to="/">
             Features
-          </NavLink>
-          <NavLink className="nav__link" exact to="/">
+          </Link>
+          <Link className="nav__link" exact to="/">
             Pricing
-          </NavLink>
-          <NavLink className="nav__link" exact to="/">
+          </Link>
+          <Link className="nav__link" exact to="/">
             Resources
-          </NavLink>
+          </Link>
 
-          <NavLink className="nav__link" exact to="/">
+          <Link className="nav__link" exact to="/">
             Login
-          </NavLink>
+          </Link>
           <Buttons
             type="round"
             size={window.innerWidth < 720 ? "1.5rem 5em" : "1rem 3rem"}
             text="Sign Up"
           />
         </div>
-        <NavLink
+        <Link
           className="nav__hamburger"
           exact
           to="/"
           onClick={this.handleClick}
         >
-          <i class={this.state.active ? "fa fa-times" : "fa fa-bars"}></i>
-        </NavLink>
+          <i className={this.state.active ? "fa fa-times" : "fa fa-bars"}></i>
+        </Link>
       </nav>
     );
   }
