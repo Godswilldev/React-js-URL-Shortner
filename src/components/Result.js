a985872ca67530c9c8c50b253b382b2620fc3294
Godswilldev <gimmex1@gmail.com>
2021-05-28 10:11:27 +0100

    styled the result

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,19 +4,21 @@ class Result extends Component {
     copy: "Copy",
   };
   handleClick = () => {
-    setTimeout(() => this.setState({ copy: "Copied" }), 100);
+    setTimeout(() => this.setState({ copy: "Copied!" }), 100);
     navigator.clipboard.writeText(this.props.finalUrl);
     setTimeout(() => this.setState({ copy: "Copy" }), 1000);
   };
 
   render() {
     return (
-      <div>
-        <h1>{this.props.initialUrl}</h1>
-        <h2>{this.props.finalUrl}</h2>
-        <button className="btn btn__square" onClick={this.handleClick}>
-          {this.state.copy}
-        </button>
+      <div className="result">
+        <h2 className="result__initialurl">{this.props.initialUrl}</h2>
+        <div className="result__result">
+          <h2 className="result__finalurl">{this.props.finalUrl}</h2>
+          <button className="btn btn__square" onClick={this.handleClick}>
+            {this.state.copy}
+          </button>
+        </div>
       </div>
     );
   }
